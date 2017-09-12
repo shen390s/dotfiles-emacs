275df8a7deb2558c3ab76f64fd4945be44afde2b
Samuel Hodgkins <samuel.hodgkins@sky.com>
2017-09-12 17:21:25 +0100

    update

--- init.el
+++ init.el
@@ -25,16 +25,45 @@
   :ensure t
   :bind (("C-x g" . magit-status)))
 
+(use-package haskell-mode
+  :ensure t
+  :init (progn (setq haskell-tags-on-save t)
+        (add-hook 'haskell-mode-hook (lambda () (ghc-init)))))
+
+(use-package ghc
+  :ensure t
+  :commands ghc-init ghc-debug
+  :config (add-hook 'haskell-mode-hook (lambda () (ghc-init))))
+
+(use-package company-ghc
+  :ensure t
+  :config (progn (add-to-list 'company-backends 'company-ghc)
+                 (setq company-ghc-show-info t)))
 (cua-mode t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((Tab-Width . 4)))))
+ '(company-backends
+   (quote
+    (company-ghc company-bbdb company-nxml company-css company-eclim company-semantic company-clang company-xcode company-cmake company-capf company-files
+                 (company-dabbrev-code company-gtags company-etags company-keywords)
+                 company-oddmuse company-dabbrev)))
+ '(flycheck-checkers
+   (quote
+    (ada-gnat asciidoc c/c++-clang c/c++-gcc c/c++-cppcheck cfengine chef-foodcritic coffee coffee-coffeelint coq css-csslint d-dmd dockerfile-hadolint elixir-dogma emacs-lisp emacs-lisp-checkdoc erlang eruby-erubis fortran-gfortran go-gofmt go-golint go-vet go-build go-test go-errcheck go-unconvert groovy haml handlebars haskell-ghc haskell-hlint html-tidy javascript-eslint javascript-jshint javascript-gjslint javascript-jscs javascript-standard json-jsonlint json-python-json less lua-luacheck lua perl perl-perlcritic php php-phpmd php-phpcs processing protobuf-protoc pug puppet-parser puppet-lint python-flake8 python-pylint python-pycompile r-lintr racket rpm-rpmlint markdown-mdl rst-sphinx rst ruby-rubocop ruby-rubylint ruby ruby-jruby rust-cargo rust scala scala-scalastyle scheme-chicken scss-lint sass/scss-sass-lint sass scss sh-bash sh-posix-dash sh-posix-bash sh-zsh sh-shellcheck slim slim-lint sql-sqlint systemd-analyze tex-chktex tex-lacheck texinfo typescript-tslint verilog-verilator xml-xmlstarlet xml-xmllint yaml-jsyaml yaml-ruby)))
+ '(safe-local-variable-values
+   (quote
+    ((2flycheck-disabled-checkers
+      (quote
+       (haskell-stack-ghc)))
+     (flycheck-disabled-checkers . haskell-stack-ghc)
+     (Tab-Width . 4)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(add-to-list 'default-frame-alist '(font . "-unknown-PragmataPro-normal-normal-normal-*-12-*-*-*-*-0-iso10646-1"))
